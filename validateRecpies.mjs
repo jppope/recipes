bb6302c377115cb7cab508b45c131b4cb5422b27
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2022-09-22 15:11:34 -0700

    added chorizo pasta

--- validateRecpies.mjs
+++ validateRecpies.mjs
@@ -30,13 +30,18 @@ import schema from './schema.json' assert {type: 'json'};
 	for (const recipe of recipes) {
 		try {
 			doc = await yaml.load(fs.readFileSync(path.join('./recipes', recipe), 'utf8'));
-			console.log(doc);
+			// console.log(doc);
 		} catch (e) {
 			console.log(e);
 		}
 
+		let price = doc.ingredients.map(ingredient => ingredient.cost).reduce((a, b) => a + b, 0);
+
+		// round price up to the nearest integer
+		price = Math.ceil(price);
+
 		// calculate the cost
-		console.log("recipe cost", doc.ingredients.map(ingredient => ingredient.cost).reduce((a, b) => a + b, 0));
+		console.log(`${doc.title}, (cost: ${price})`);
 
 		const valid = ajv.validate(schema, doc);
 		if (!valid) {
